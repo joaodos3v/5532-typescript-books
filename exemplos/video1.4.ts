596a12efcb1c888d2802b6981e07bbf2051c223d
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-12-06 12:21:21 -0300

    Vídeo 1.4 - Tipos Primitivos e Inferência/Tipagem Explícita

---
+++ exemplos/video1.4.ts
@@ -0,0 +1,10 @@
+// TYPESCRIPT
+let nomeLivro: string = "Fundamentos do Typescript"; // Tipagem Explícita
+let idTarefa = 1; // Tipagem Implícita / Inferência
+let concluida: boolean = false;
+
+// Tipos primitivos
+// string, number, boolean
+
+nomeLivro = 10;
+
