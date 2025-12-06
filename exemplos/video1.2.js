4045fb06772d954e27dba88c98b8269a80d26a0d
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-12-06 11:39:32 -0300

    Vídeo 1.2 - Apresentando o Typescript com um exemplo de soma

---
+++ exemplos/video1.2.js
@@ -0,0 +1,11 @@
+function soma(numero1, numero2) {
+  return numero1 + numero2;
+}
+
+// Chamadas
+const resultado1 = soma(1, 2);
+const resultado2 = soma(1, '2');
+
+// Exibir
+console.log(resultado1);
+console.log(resultado2);
